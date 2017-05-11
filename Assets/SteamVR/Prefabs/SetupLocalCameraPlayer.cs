f2459d83bb7bbaf631a6ed0fdb5980e5d3d41bca
Andrea Longo <andrea.longo@framestore.com>
2017-05-11 10:35:02 -0700

    initial commit

---
+++ Assets/SteamVR/Prefabs/SetupLocalCameraPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SetupLocalCameraPlayer : NetworkBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        if (isLocalPlayer)
+        {
+            GetComponent<drive>().enabled = true;
+            GetComponent<WASDQE>().enabled = true;
+            GetComponentInChildren<Camera>().enabled = true;
+        }
+    }
+}
---
+++ Assets/SteamVR/Prefabs/WASDQE.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class WASDQE : MonoBehaviour
+{
+
+    public GameObject head;
+    float Y;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        Y = this.transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        this.transform.position = new Vector3(transform.position.x, Y, transform.position.z);
+
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.transform.Translate(head.transform.forward * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.Translate(-head.transform.right * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.transform.Translate(-head.transform.forward * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.Translate(head.transform.right * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            Y -= Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            Y += Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.J))
+        {
+            this.transform.rotation = new Quaternion(this.transform.rotation.x, this.transform.rotation.y + (90 * Time.deltaTime), this.transform.rotation.z, 0);
+        }
+
+
+
+    }
+
+}
