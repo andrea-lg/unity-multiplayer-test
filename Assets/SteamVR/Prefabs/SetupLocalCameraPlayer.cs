e7d73b02dce7ac08a25b59742573b0fe76e467d6
Andrea Longo <andrealongo22@gmail.com>
2017-05-18 11:43:18 -0700

    Added prefab spawn with collision

--- Assets/SteamVR/Prefabs/SetupLocalCameraPlayer.cs
+++ Assets/SteamVR/Prefabs/SetupLocalCameraPlayer.cs
@@ -5,6 +5,8 @@ using UnityEngine.Networking;
 
 public class SetupLocalCameraPlayer : NetworkBehaviour
 {
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
     // Use this for initialization
     void Start()
     {
@@ -20,6 +22,35 @@ public class SetupLocalCameraPlayer : NetworkBehaviour
         foreach (controllerScript obj in controllers)
         {
             obj.gameObject.SetActive(true);
+        }
+    }
+
+    [Command]
+    void CmdFire()
+    {
+        // Create the Bullet from the Bullet Prefab
+        var bullet = (GameObject)Instantiate(
+            bulletPrefab,
+            bulletSpawn.position,
+            bulletSpawn.rotation);
+
+        // Add velocity to the bullet
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+
+        // Destroy the bullet after 2 seconds
+        Destroy(bullet, 2.0f);
+    }
+
+    void Update()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CmdFire();
         }
     }
 }
